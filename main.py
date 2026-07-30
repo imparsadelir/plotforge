7f1fbf20198991992aa13244c0d911e7bd1c8046
Parsa <delirparsa@gmail.com>
2026-07-30 12:35:27 +0330

    feat: load data files and plot columns from GUI

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import sys
 import numpy as np
-from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
+from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget , QFileDialog , QPushButton
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
-
+from core.io.readers import read_data_file
 
 class PlotCanvas(FigureCanvasQTAgg):
     def __init__(self):
@@ -22,20 +22,40 @@ class MainWindow(QMainWindow):
 
         container = QWidget()
         layout = QVBoxLayout(container)
-        layout.addWidget(self.canvas)
         self.setCentralWidget(container)
+        
+        button = QPushButton("open File")
+        button.clicked.connect(self.open_file)
+        layout.addWidget(self.canvas)
+        layout.addWidget(button)
+        
+    def open_file(self):
+        file_path, _ = QFileDialog.getOpenFileName(
+            self,
+            "Open Data File",
+            "",
+            "Data Files (*.xlsx *.xls *.csv)"
+        )
 
-        self.draw_test_plot()
+        if not file_path:
+            return
 
-    def draw_test_plot(self):
-        x = np.linspace(0, 10, 200)
-        self.canvas.axes.plot(x, np.sin(x), label="sin(x)")
-        self.canvas.axes.plot(x, np.cos(x), "--", label="cos(x)")
-        self.canvas.axes.set_xlabel("x")
-        self.canvas.axes.set_ylabel("y")
+        df = read_data_file(file_path)
+
+        self.canvas.axes.clear()
+
+        columns = list(df.columns)
+        x_column = columns[0]
+        y_columns = columns[1:]
+
+        for col in y_columns:
+            self.canvas.axes.plot(df[x_column], df[col], label=col)
+
+        self.canvas.axes.set_xlabel(x_column)
         self.canvas.axes.legend()
-        self.canvas.axes.grid(alpha=0.3)
+        self.canvas.axes.grid(alpha=0.9)
         self.canvas.draw()
+        
 
 
 if __name__ == "__main__":
@@ -43,4 +63,5 @@ if __name__ == "__main__":
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
+    
     
